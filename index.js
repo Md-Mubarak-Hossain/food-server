52e14941c12374ec4833f6eda6e42626af8ae7d7
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2023-01-06 08:21:33 +0600

    order and booking section added

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+// const recipes=require("recipes.json()")
+const port = process.env.PORT || 5001;
 require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
+// app.get('/foods', (req, res) => {
+//     res.send(recipes)
+// })
 app.get('/', (req, res) => {
-    res.send(`server run on ${port}`)
+    res.send(`BenZen Food recipes server run`)
 })
 app.listen(port, () => {
-    console.log('port', port)
+    console.log('BenZen Food recipes server port', port)
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@librarycluster.iklz7tv.mongodb.net/?retryWrites=true&w=majority`;
@@ -19,30 +23,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const database = client.db("OnlineLibrary");
-        const bookCollection = database.collection("books");
-        const userCollection = database.collection("user");
+        const database = client.db("BeZenFoodRecipes");
+        const recipeCollection = database.collection("recipes");
+        const bookingCollection = database.collection("booking");
+        const userCollection = database.collection("users");
 
-        app.get("/creator/:id", async (req, res) => {
+        app.get("/users/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await userCollection.findOne(query);
             res.send(result);
         })
 
-        app.get("/creator", async (req, res) => {
+        app.get("/users", async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.post('/creator', async (req, res) => {
+        app.post('/users', async (req, res) => {
             const file = req.body;
             const result = await userCollection.insertOne(file);
             res.send(result);
         })
 
-        app.patch('/creator/:id', async (req, res) => {
+        app.patch('/users/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const updateFile = req.body;
@@ -57,57 +62,104 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/creator/:id', async (req, res) => {
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
-        app.get("/paper", async (req, res) => {
+        app.get("/recipes", async (req, res) => {
             const query = {};
-            const cursor = bookCollection.find(query);
+            const cursor = recipeCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get("/paper/:id", async (req, res) => {
+        app.get("/recipes/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
-            const result = await bookCollection.findOne(query);
+            const result = await recipeCollection.findOne(query);
             res.send(result);
         })
 
-        app.get("/bookEmail/:email", async (req, res) => {
+        app.get("/recipeEmail/:email", async (req, res) => {
             const query = { email: req.params.email };
-            const result = await bookCollection.findOne(query);
+            const result = await recipeCollection.findOne(query);
             res.send(result);
         })
 
-        app.post('/paper', async (req, res) => {
+        app.post('/recipes', async (req, res) => {
             const file = req.body;
-            const result = await bookCollection.insertOne(file);
+            const result = await recipeCollection.insertOne(file);
             res.send(result);
         })
 
-        app.patch('/paper/:id', async (req, res) => {
+        app.patch('/recipes/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
-            const updateFile = req.body;
+            const updateRecipes = req.body;
             const updateDoc = {
                 $set: {
-                    name: updateFile.name,
-                    url: updateFile.url,
-                    writer: updateFile.writer
+                    name: updateRecipes.name,
+                    price: updateRecipes.price,
+                    description: updateRecipes.description
 
                 }
             }
-            const result = await bookCollection.updateOne(filter, updateDoc);
+            const result = await recipeCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
 
-        app.delete('/paper/:id', async (req, res) => {
+        app.delete('/recipes/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await bookCollection.deleteOne(query);
+            const result = await recipeCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.get("/orders", async (req, res) => {
+            const query = {};
+            const cursor = bookingCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await bookingCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.get("/orderEmail/:email", async (req, res) => {
+            const query = { email: req.params.email };
+            const result = await bookingCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/orders', async (req, res) => {
+            const file = req.body;
+            const result = await bookingCollection.insertOne(file);
+            res.send(result);
+        })
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updateorders = req.body;
+            const updateDoc = {
+                $set: {
+                    name: updateorders.name,
+                    price: updateorders.price,
+                    description: updateorders.description
+
+                }
+            }
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
             res.send(result);
         })
     }
